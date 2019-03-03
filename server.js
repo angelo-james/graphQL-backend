678886956ecba21d3541e3ed5fe55e1f882c3734
AJ Arriola <angelojames.arriola@gmail.com>
2019-03-02 22:02:08 -0800

    implemented express-graphql

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema');
 const port = 3800;
+
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema:schema,
+  graphiql:true
+}));
 
 app.listen(port, () => {
   console.log(`You're listening on port ${port}...`)
